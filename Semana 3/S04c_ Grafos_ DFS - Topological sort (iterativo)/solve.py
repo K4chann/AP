56892045ee6007040a229fbffe6218bb9795b1f4
K4chann <124625996+K4chann@users.noreply.github.com>
2023-09-26 09:47:34 +0100

    Update solve.py

--- Semana 3/S04c_ Grafos_ DFS - Topological sort (iterativo)/solve.py
+++ Semana 3/S04c_ Grafos_ DFS - Topological sort (iterativo)/solve.py
@@ -28,25 +28,29 @@ def dfs_topological_sort(G: nx.DiGraph):
     # solve it here! ------------------------------------------------
 
     def dfs_iterative(u):
-        nonlocal N
-        #  1. Añade código aqui
+        nonlocal N # Es necesario definir N como "nonlocal", ya que a la hora de usar una varibale con su mismo nombre, ésta se reinicializa
+        #  1. Añade código aqui # No es necesario definir el resto de variables utilizadas como "s" (Stack()) u "order" (dict()) ya que solo estamos usando métodos en ellas, no asignando valores
         s.push(u)
         
         while not s.isEmpty():
             
-            node = s.peek()
+            node = s.peek() # Usamos peek() para no sacar el elemento de la pila todavía, sino cuando sea absolutamente necesaria su asignación en el orden topológico
             visibleNodes.add(node)
-            
+
+            # Si el nodo visualizado tiene sucesores no visualizados, se añaden a la pila
             for neighbor in G.successors(node):
                 if neighbor not in visibleNodes:
                     s.push(neighbor)
-            
+
+            # Si el último elemento de la pila es el mismo elemento que hemos mirado al principio de la iteración,
+            # significa que ese nodo no tiene más sucesores, por lo que podemos asignarle un orden
             if s.peek() == node:
                 order[s.pop()] = N
                 N -= 1
 
     #  2. Añade código también aqui
     s = Stack()
+    # Recorremos todos los nodoa del grafo y llamamos a la función dfs_iterative(node) para procesarlo
     for node in G:
         if node not in visibleNodes:
             dfs_iterative(node)
