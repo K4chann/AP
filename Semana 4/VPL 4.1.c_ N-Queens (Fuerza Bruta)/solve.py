8c2cd8f2c05439c888ca5dabd5776e9d0d2300b2
K4chann <124625996+K4chann@users.noreply.github.com>
2023-10-03 09:09:59 +0100

    Update solve.py

--- Semana 4/VPL 4.1.c_ N-Queens (Fuerza Bruta)/solve.py
+++ Semana 4/VPL 4.1.c_ N-Queens (Fuerza Bruta)/solve.py
@@ -17,12 +17,17 @@ def solve(num_queens):
 
     def check_solution(c):
 
+        # Itero por índices la lista pasada como parámetro (una combinaicón)
         for queen_pos in range(len(c)):
+            # Manteniendo la posición de la reina, itero nuevamente el resto de reinas posteriores
             for other_q_pos in range(queen_pos + 1, len(c)):
+                # Compruebo que la reina2 no esté en la misma columna o diagonal que la reina1
                 if c[queen_pos] == c[other_q_pos] or\
                 abs(c[queen_pos] - c[other_q_pos]) == other_q_pos - queen_pos:
+                    # Las reinas están en la misma columna o en la misma diagonal, retorno False
                     return False
-                
+
+        # He acabado de iterar las reinas y no he tenido ningún problema, itero True
         return True
             
     solutions_list = []
@@ -30,9 +35,13 @@ def solve(num_queens):
     combination_iterator = My_Iterator(num_queens, num_queens)
 
     # solve it here!    
+    # Iteramos todas las combinaciones posibles
     for combination in combination_iterator.next():
+        # Llamo a check_solution() para verificar el resultado
         if check_solution(combination):
+            # Comprobamos si la solución tiene un tamaño adecuado (no es necesario)
             if len(set(combination)) == num_queens:
+                # Añado la combinación al resto de soluciones
                 solutions_list.append(combination)
 
     return solutions_list
